028702dbd0204f1e66de6f08d3d60d27b3e7a773
natinael <natnaelmasresha2022@gmail.com>
2023-11-21 12:06:17 +0300

    New game created word reverse

---
+++ games/WordReverse.java
@@ -0,0 +1,19 @@
+package games;
+
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ * WordReverse
+ */
+public class WordReverse {
+
+    public static void main(String[] args) {
+    Greetings();
+    }
+
+    public static void Greetings(){
+        System.out.println("\t !!!!!!!! Welcome to The Game !!!!!!!");
+        System.out.println("\t  >>>>>>I hope that you have fun her<<<<<<<\n");
+    }
+}
